71bfd64ae3bc388a9d1146a1a315da2b9582e4b2
Jenna Goldman <jenna.goldman89@gmail.com>
2020-01-22 10:17:42 -0800

    add passing test of createPokemon function

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,29 @@
 // IMPORT MODULES under test here:
+import { createPokemons } from '../products/create-pokemons.js';
 // import example from '../src/example.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('pikachu data renders as formatted and styled list item', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const pikachu = {
+        id: 'pikachu',
+        name: 'Pikachu',
+        image: 'assets/pikachu.png',
+        description: 'Electric-type, mouse Pokemon',
+        category: 'electric',
+        price: 50,
+    };
 
+    const expected = '<li class="electric" title="Electric-type, mouse Pokemon"><h3>Pikachu</h3><img src="../assets/pikachu.png"><p>$50.00</p><button value="pikachu">Add</button></li>';
+
+    const pikachuListItem = createPokemons(pikachu);
+    const result = pikachuListItem.outerHTML;
     //Act 
     // Call the function you're testing and set the result to a const
 
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(expected, result);
 });
