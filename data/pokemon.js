4f66dbe9e24963fd2bbb76bfc0cb54b9e07549f6
Jenna Goldman <jenna.goldman89@gmail.com>
2020-01-21 16:36:24 -0800

    add index.html and products.html basic structure, and add pokemon.js data

---
+++ data/pokemon.js
@@ -0,0 +1,96 @@
+const bulbasaur = {
+    id: '001',
+    name: 'Bulbasaur',
+    image: 'assets/bulbasaur.png',
+    description: 'Grass-type, seed Pokemon',
+    category: 'grass',
+    price: 10,
+};
+
+const charmander = {
+    id: '004',
+    name: 'Charmander',
+    image: 'assets/charmander.png',
+    description: 'Fire-type, lizard Pokemon',
+    category: 'fire',
+    price: 10,
+};
+
+const squirtle = {
+    id: '007',
+    name: 'Squirtle',
+    image: 'assets/squirtle.png',
+    description: 'Water-type, tiny turtle Pokemon',
+    category: 'water',
+    price: 10,
+};
+
+
+const pikachu = {
+    id: '025',
+    name: 'Pikachu',
+    image: 'assets/pikachu.png',
+    description: 'Electric-type, mouse Pokemon',
+    category: 'electric',
+    price: 10,
+};
+
+const jigglypuff = {
+    id: '039',
+    name: 'Jigglypuff',
+    image: 'assets/jigglypuff.png',
+    description: 'Fairy-type, balloon Pokemon',
+    category: 'fairy',
+    price: 10,
+};
+
+const eevee = {
+    id: '133',
+    name: 'Eevee',
+    image: 'assets/eevee.png',
+    description: 'Normal-type, evolution Pokemon',
+    category: 'normal',
+    price: 10,
+};
+
+const vaporeon = {
+    id: '134',
+    name: 'Vaporeon',
+    image: 'assets/vaporeon.png',
+    description: 'Water-type, bubble jet Pokemon',
+    category: 'normal',
+    price: 10,
+};
+
+const jolteon = {
+    id: '135',
+    name: 'Jolteon',
+    image: 'assets/jolteon.png',
+    description: 'Electric-type, lightning Pokemon',
+    category: 'normal',
+    price: 10,
+};
+
+const flareon = {
+    id: '136',
+    name: 'Flareon',
+    image: 'assets/flareon.png',
+    description: 'Fire-type, flame Pokemon',
+    category: 'normal',
+    price: 10,
+};
+
+const pokemon = [
+    bulbasaur,
+    charmander,
+    squirtle,
+    pikachu,
+    jigglypuff,
+    eevee,
+    vaporeon,
+    jolteon,
+    flareon
+];
+
+export default pokemon;
+
