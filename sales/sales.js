c34c5cdf93eaed4c6422607f9a6a46100a1684c4
Jenna Goldman <jenna.goldman89@gmail.com>
2020-02-02 14:30:50 -0800

    render sales history data in table rows

--- sales/sales.js
+++ sales/sales.js
@@ -1,5 +1,13 @@
 import { renderSalesHistoryChart } from './render-sales-history-chart.js';
+import { renderSalesHistoryTable } from './render-sales-history-table.js';
 
+const tableBody = document.getElementById('table-body');
 const salesHistory = JSON.parse(localStorage.getItem('sales-history'));
 
 renderSalesHistoryChart(salesHistory);
+
+
+salesHistory.forEach(item => {
+    const renderedLine = renderSalesHistoryTable(item);
+    tableBody.appendChild(renderedLine);
+});
